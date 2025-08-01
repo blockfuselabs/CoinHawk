37f00460825ee609b14c44d008accccd4a2315d6
VUG_SEN_22_7193 <7193@veritas.edu.ng>
2025-08-01 09:34:21 +0100

    final update

--- src/utils/reusableCache.ts
+++ src/utils/reusableCache.ts
@@ -27,27 +27,7 @@ class Cache{
 }
 
 const cache = new Cache();
-export default cache
+export default cache;
 
-// const cache = new NodeCache({ stdTTL: 500 });
-
-// function setToCache(key, value) {
-//   cache.set(key, value);
-// }
-
-
-// function getFromCache(key) {
-//   return cache.get(key);
-// }
-
-// function deleteFromCache(key) {
-//   cache.del(key);
-// }
-
-// module.exports = {
-//   setToCache,
-//   getFromCache,
-//   deleteFromCache
-// };
 
 
